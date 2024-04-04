49a4fc26d4f80d670906ab95ce88db96696fb604
César Ordóñez <ceordonezv@gmail.com>
2024-04-04 18:54:16 +0200

    new stuff

--- main_plots.py
+++ main_plots.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-from scr.read_data import read_config, read_model, read_inputs_meteo
+from scr.read_data import read_config, read_model, read_inputs_meteo, read_obs
 from scr.plot_model import plot_model, ts_colormesh, plot_timeserie
 from scr.processing_data import process_model
 from scr.write_data import write_data
@@ -14,6 +14,8 @@ def main():
 
     cfg = read_config('config_plots.yml')
     #idata = read_inputs_meteo(cfg)
+    obsdata = read_obs(cfg)
+    __import__('pdb').set_trace()
     modeldata = read_model(cfg)
     modeldata = process_model(cfg, modeldata)
     plot_model(cfg, modeldata)
