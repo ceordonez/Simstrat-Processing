c85d34c7881d19618eb8df5490904b7e95e1f002
César Ordóñez <ceordonezv@gmail.com>
2024-04-23 18:12:32 +0200

    small chages to do tendency analysis for air temperature

--- analysis/functions.py
+++ analysis/functions.py
@@ -1,20 +1,36 @@
+import pandas as pd
 
+def normalize_data(data, loc, var, period=''):
 
-def normalize_data(data, loc, var):
+    if period == 'all':
+        newdata = normalize_data(data, None, var, period='')
+        return newdata
+    elif period == 'both':
+        p1data = data.loc[:loc]
+        p1newdata = normalize_data(p1data, None, var, period='')
+        p2data = data.loc[loc:]
+        nloc = p1data.index[0]
+        p2newdata = normalize_data(p2data, None, var, period='')
+        newdata = pd.concat([p1newdata, p2newdata])
+        return newdata
+    else:
+        datam = data[var].resample('ME').mean()
+        if loc is None:
+            loc = datam.index[12]
+        meanp1 = datam[:loc].mean()
+        newdata = data.copy()
+        aux1 = []
+        aux2 = []
+        for year in datam.loc[loc:].index.year.unique():
+            mmeany = datam.loc[str(year)].mean()
+            mmeany0 = datam.loc[str(loc.year-1):str(year)].mean()
+            aux1.append(meanp1/mmeany*meanp1)
+            aux2.append(mmeany/mmeany0*meanp1)
+            newdata.loc[str(year), var] = (meanp1/mmeany)*(mmeany/mmeany0)*newdata.loc[str(year), var]
 
-    datam = data[var].resample('ME').mean()
-    meanp1 = datam.loc[:loc].mean()
-    newdata = data.copy()
-    aux1 = []
-    aux2 = []
-    for year in datam.loc[loc:].index.year.unique():
-        mmeany = datam.loc[str(year)].mean()
-        mmeany0 = datam.loc[str(loc.year-1):str(year)].mean()
-        aux1.append(meanp1/mmeany*meanp1)
-        aux2.append(mmeany/mmeany0*meanp1)
-        newdata.loc[str(year), var] = (meanp1/mmeany)*(mmeany/mmeany0)*newdata.loc[str(year), var]
-    newdata.loc[loc:, var] = meanp1/newdata.loc[loc:, var].mean()*newdata.loc[loc:, var]
-    return newdata
+        newdata.loc[loc:, var] = meanp1/newdata.loc[loc:, var].mean()*newdata.loc[loc:, var]
+
+        return newdata
 
 def select_season(data, season):
     data.reset_index(inplace=True)
--- analysis/tendency_analysis.py
+++ analysis/tendency_analysis.py
@@ -21,10 +21,13 @@ plt.style.use('~/.config/matplotlib/aslo-paper.mplstyle')
 PATHOUT ='/home/cesar/Dropbox/Cesar/PostDoc/Projects/WaterClarity/Simulations/INPUTS/HALLWIL/'
 #VAR = 'I_RAD'
 #NAMEVAR = 'Solar radiation [W/m^2]'
+#NORMPERIOD = ''
 #VAR = 'I_VAP'
 #NAMEVAR = 'Vapour pressure [mbar]'
+#NORMPERIOD = ''
 VAR = 'I_ATEMP'
 NAMEVAR = 'Temperature [degC]'
+NORMPERIOD = 'all'
 SEASON = ''
 
 cfg = read_varconfig('../config_preprocessing.yml')
@@ -45,7 +48,8 @@ loc = pd.to_datetime(respt.cp)
 mu1 = respt.avg.mu1
 mu2 = respt.avg.mu2
 
-newdata = fn.normalize_data(inputdata, loc, VAR)
+newdata = fn.normalize_data(inputdata, loc, VAR, NORMPERIOD)
+__import__('pdb').set_trace()
 newdatam = newdata[VAR].resample('ME').mean()
 newdatay = newdata[VAR].resample('YE').mean()
 mu3 = newdata.loc[loc:, VAR].mean()
@@ -62,10 +66,10 @@ if SEASON == '':
     trend2 = np.arange(len(datam[loc:]))/PERIOD*resst2.slope + resst2.intercept
 
     print('SEASONAL TEST USING MONTLY DATA')
-    print('Trend entire period:', resst1.trend, 'p-value:', resst0.p, 'Average:', inputdata.loc[:, VAR].mean(), 'Slope:', resst0.slope/PERIOD)
-    print('Trend until:', loc, resst1.trend, 'p-value:', resst1.p, 'Average:', inputdata.loc[:loc, VAR].mean(), 'Solpe:', resst1.slope/PERIOD)
-    print('Trend from:', loc, resst2.trend, 'p-value:', resst2.p, 'Average:', inputdata.loc[loc:, VAR].mean(), 'Slope:', resst2.slope/PERIOD)
-    print('New trend from:', loc, resst3.trend, 'p-value:', resst3.p, 'Average:', newdata.loc[loc:, VAR].mean(), 'Slope:', resst3.slope/PERIOD)
+    print('Trend entire period:', resst1.trend, 'p-value:', resst0.p, 'Average:', inputdata.loc[:, VAR].mean(), 'Slope per year:', resst0.slope)
+    print('Trend until:', loc, resst1.trend, 'p-value:', resst1.p, 'Average:', inputdata.loc[:loc, VAR].mean(), 'Solpe per year:', resst1.slope)
+    print('Trend from:', loc, resst2.trend, 'p-value:', resst2.p, 'Average:', inputdata.loc[loc:, VAR].mean(), 'Slope per year:', resst2.slope)
+    print('New trend from:', loc, resst3.trend, 'p-value:', resst3.p, 'Average:', newdata.loc[loc:, VAR].mean(), 'Slope per year:', resst3.slope)
 
     pl.plot_ts(inputdata, newdata, VAR, loc, respt, mu3, resst1, resst2, PERIOD, SEASON, cfg)
 
@@ -75,12 +79,13 @@ res2 = mk.original_test(datay[loc:])
 res3 = mk.original_test(newdatay[loc:])
 
 print('ORIGINAL TEST USING YEARLY DATA')
-print('Trend entire period:', res1.trend, 'Average:', inputdata.loc[:, VAR].mean(), 'Slope:', res0.slope)
-print('Trend until:', loc, res1.trend, 'Average:', inputdata.loc[:loc, VAR].mean(), 'Slope:', res1.slope)
-print('Trend from:', loc, res2.trend, 'Average:', inputdata.loc[loc:, VAR].mean(), 'Slope:', res2.slope)
-print('New trend from:', loc, res3.trend, 'Average:', newdata.loc[loc:, VAR].mean(), 'Slope:', res3.slope)
+print('Trend entire period:', res1.trend, 'Average:', inputdata.loc[:, VAR].mean(), 'Slope per year:', res0.slope)
+print('Trend until:', loc, res1.trend, 'Average:', inputdata.loc[:loc, VAR].mean(), 'Slope per year:', res1.slope)
+print('Trend from:', loc, res2.trend, 'Average:', inputdata.loc[loc:, VAR].mean(), 'Slope per year:', res2.slope)
+print('New trend from:', loc, res3.trend, 'Average:', newdata.loc[loc:, VAR].mean(), 'Slope per year:', res3.slope)
 pl.plot_ts(inputdata, newdata, VAR, loc, respt, mu3, res1, res2, 1, SEASON, cfg)
 
 plt.show()
 
+__import__('pdb').set_trace()
 #wr.write_forcing(cfg, newdata, VAR, PATHOUT)
